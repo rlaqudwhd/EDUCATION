211f9904476c52d90f342361b7406be48a4af428
rlaqudwhd <52453306+rlaqudwhd@users.noreply.github.com>
2019-07-22 15:04:58 +0900

    Update 씹어먹는 C언어 01.c

--- C/씹어먹는 C언어 01.c
+++ C/씹어먹는 C언어 01.c
@@ -1,6 +1,6 @@
 #include <stdio.h> // stdio.h 라는 헤더파일을 포함한다, stdio.h = STandard input output header
 int main() // int는 정수로 integer의 약자, main은 함수 정수 형을 반환하는 메인 함수, main은 시작을 알리는 함수이다
 {
-    printf("Hello World!\n"); // printf
+    printf("Hello World!\n");
     return0; //0을 반환 하는 이유는  컴퓨터에게 무사히 종료 되었음을 알리는 것이다 반면 1을 반환하면 오류가 있다는 뜻이다
 }
